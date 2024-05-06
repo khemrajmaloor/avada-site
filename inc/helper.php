6b8693a112ada36d0cc305eddb4e194747b9ef17
khemraj <khemraj@rwit.io>
2024-05-06 11:31:36 +0530

    Enqueue main.css file in helper

--- inc/helper.php
+++ inc/helper.php
@@ -2,4 +2,15 @@
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
+
+
+// Enqueue styles script Avada theme ..
+if (!function_exists('avada_enqueue_styles_scripts')) {
+    function avada_enqueue_styles_scripts() {
+        wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/main.css' );
+    }
+    add_action( 'wp_enqueue_scripts', 'avada_enqueue_styles_scripts' );
+}
+
+
 ?>
