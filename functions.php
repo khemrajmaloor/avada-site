99f7429db597066e3ba136c61fe71b7ef5102dba
khemraj <khemraj@rwit.io>
2024-05-06 11:04:10 +0530

    Enqueue main css  file

--- functions.php
+++ functions.php
@@ -2,4 +2,14 @@
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
+
+
+
+// Avada enqueue styles script..
+function avada_enqueue_styles_script() {
+	wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/main.css' );
+}
+add_action( 'wp_enqueue_scripts', 'avada_enqueue_styles_script' );
+
+
 ?>
