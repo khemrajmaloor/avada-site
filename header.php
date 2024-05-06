dedd56aa6ce6eb1ea92defee948741358be063a2
khemraj <khemraj@rwit.io>
2024-05-06 11:04:29 +0530

    Added  header in this fucntion

--- header.php
+++ header.php
@@ -3,3 +3,14 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>" />
+        <title><?php bloginfo('name'); ?></title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class();?>>
+        
+    
