2cea82d241034c9018dbd92b78f0918336b25edb
khemraj <khemraj@rwit.io>
2024-05-06 11:30:45 +0530

    include helper file in function file

--- functions.php
+++ functions.php
@@ -4,12 +4,7 @@ if (!defined('ABSPATH')) {
 }
 
 
-
-// Avada enqueue styles script..
-function avada_enqueue_styles_script() {
-	wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/main.css' );
-}
-add_action( 'wp_enqueue_scripts', 'avada_enqueue_styles_script' );
+require_once('inc/helper.php');
 
 
 ?>
